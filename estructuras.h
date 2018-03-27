9ef4ce07ee36a5a152247603d84d8ef51f6b8792
AdanMora <ajmorafallas@gmail.com>
2018-03-27 14:48:46 -0700

    Cola Ready Lista

--- estructuras.h
+++ estructuras.h
@@ -1,1 +1,38 @@
-holi
+#ifndef ESTRUCTURAS
+#define ESTRUCTURAS
+
+typedef struct NodoJobScheduler NodoJobScheduler;
+typedef struct ListaColaReady ListaColaReady;
+
+typedef struct NodoJobScheduler{
+    int burst;
+	int prioridad;
+    int PID;
+
+    NodoJobScheduler * siguiente;
+	NodoJobScheduler * anterior;
+	
+} NodoJobScheduler;
+
+typedef struct ListaColaReady{
+	NodoJobScheduler * primerNodo;
+	
+} ListaColaReady;
+ 
+void insertar(int, int, int, ListaColaReady *);
+
+int largoLista(ListaColaReady *);
+
+NodoJobScheduler* extraerPID(ListaColaReady *);
+
+NodoJobScheduler* extraerBurst(ListaColaReady *);
+
+NodoJobScheduler* extraerPrioridad(ListaColaReady *);
+
+bool isListaEmpty(ListaColaReady *);
+
+void imprimirLista(ListaColaReady *);
+
+NodoJobScheduler* borrarEnPosicion(int, ListaColaReady *);
+ 
+#endif /* ESTRUCTURAS */
